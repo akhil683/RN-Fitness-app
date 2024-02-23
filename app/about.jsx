69e561507abf541da8c3fb59f5f746bf5fd82b86
akhil683 <onkar.palsra@gmail.com>
2024-02-24 00:15:19 +0530

    inital commit

---
+++ app/about.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function About() {
+  return (
+    <View className="bg-black">
+      <Text>About</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({})
---
+++ components/ImageSlider.jsx
@@ -0,0 +1,44 @@
+import { Text, View } from 'react-native'
+import { widthPercentageToDP } from 'react-native-responsive-screen'
+import Carousel, { ParallaxImage } from 'react-native-snap-carousel'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+
+export default function ImageSlider() {
+  const sliderImages = [
+   require("../assets/images/parallax.jpg"),
+   require("../assets/images/parallax.jpg"),
+   require("../assets/images/parallax.jpg"),
+   require("../assets/images/parallax.jpg"),
+  ] 
+
+  return (
+   <Carousel
+      data={sliderImages}
+      loop={true}
+      autoplay={true}
+      renderItem={ItemCard}
+      hasParallaxImages={true}
+      sliderWidth={wp(100)}
+      firstItem={1}
+      autoplayInterval={4000}
+      itemWidth={wp(100)-70}
+      slideStyle={{display: 'flex', alignItems: 'center'}}
+   />
+  )
+}
+
+const ItemCard = ({ item, index}, parallaxProps) => {
+   return (
+      <View
+         style={{width: wp(100)-70, height: hp(25)}} 
+      >
+         <ParallaxImage 
+            source={item}
+            containerStyle={{borderRadius: 30, flex: 1}}
+            style={{resizeMode: 'contain'}}
+            parallaxFactor={1}
+            {...parallaxProps}
+         />
+      </View>
+   )
+}
